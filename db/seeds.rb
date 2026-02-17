e246cc1b4f7caa76b851df436288fa9dfc8949da
kanakitagawa1118-dotcom <kana.kitagawa1118@gmail.com>
2026-02-17 17:40:02 +0900

    tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,18 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-[
-  { name: '重い', tag_type: 'mood' },
-  { name: '普通', tag_type: 'mood' },
-  { name: '軽い', tag_type: 'mood' },
-  { name: '勉強', tag_type: 'category' },
-  { name: '仕事', tag_type: 'category' },
-  { name: '就活', tag_type: 'category' },
-  { name: '家事', tag_type: 'category' },
-  { name: '趣味', tag_type: 'category' },
-  { name: '運動', tag_type: 'category' },
-  { name: '人間関係', tag_type: 'category' },
-  { name: '健康', tag_type: 'category' },
-  { name: 'クリエイティブ', tag_type: 'category' },
-  { name: 'その他', tag_type: 'category' }
-].each do |tag|
-  record = Tag.find_or_initialize_by(name: tag[:name])
-  record.update!(tag_type: tag[:tag_type])
-end
-
-Tag.select(:name).distinct.each do |tag|
-  Tag.where(name: tag.name).offset(1).destroy_all
-end
+Tag.create([
+  { name: '重い' },
+  { name: '普通' },
+  { name: '軽い' },
+  { name: '勉強' },
+  { name: '就活' },
+  { name: '家事' },
+  { name: '仕事' },
+  { name: '趣味' },
+  { name: '運動' },
+  { name: '人間関係' },
+  { name: '健康' },
+  { name: 'クリエイティブ' },
+  { name: 'その他' },
+])
